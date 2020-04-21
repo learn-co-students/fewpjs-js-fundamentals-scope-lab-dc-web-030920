195d557bc06a4e716437fd74a1af5403212ced31
github username <github email address>
2020-04-21 00:40:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-animal = ""
+const animal = "cat";
 
 function myAnimal() {
+  let animal = "dog";
   return animal
 }
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+  // P.S.: You can't just hard-code 'cat' belo
+
   return animal
 }
 
 function add2(n) {
+  let two = 2;
   return n + two
 
   // Feel free to move things around!
-  two = 2
+
 }
